47f10a990f253357383b8808dd065edeb7665599
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2022-11-03 17:56:04 +0530

    Time: 5 ms (17.76%), Space: 6 MB (83.22%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.cpp
+++ 0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -5,13 +5,27 @@ public:
         int score = 0;
         for(int i = 0; i < s.size(); i++){
             if(s[i] == '('){
-                st.push(score);
-                score = 0;
+                st.push(-1);
             }
             else {
-                score = st.top() + max(2 * score, 1);
+                score=0;
+                while(st.top()!=-1)
+                {
+                    score+=st.top();
+                    st.pop();
+                }
                 st.pop();
+                score*=2;
+                if(score==0)
+                    score=1;
+                st.push(score);
             }
+        }
+        score=0;
+        while(!st.empty())
+        {
+            score+=st.top();
+            st.pop();
         }
         return score;
     }
