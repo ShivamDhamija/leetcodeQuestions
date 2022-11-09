dbb253bf85780fc98f6a8907b6075c705131347b
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2022-11-09 18:51:57 +0530

    Time: 7 ms (53.77%), Space: 6.9 MB (12.88%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        queue<char>st;
+        int c=0;
+        string ans="";
+        for(int i=0;i<s.size();i++)
+        {
+                st.push(s[i]);
+            if(s[i]==')')
+                c+=2;
+            if(!st.empty()&&(c==st.size()))
+            {
+                st.pop();
+                while(st.size()>1)
+                    ans.push_back(st.front()),st.pop();
+                st.pop();
+                c=0;
+            }
+            
+        }
+        return ans;
+    }
+};
