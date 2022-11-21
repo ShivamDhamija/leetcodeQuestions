e203b980919202f37eb200d5d51377f9bea0c7e5
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2022-11-21 20:07:05 +0530

    Time: 7 ms (86.00%), Space: 7.6 MB (48.58%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -1,37 +1,11 @@
 class Solution {
 public:
-    int equalSubstring(string s, string t, int ma) {
-        queue<int>st;
-        int no=0,j=0,i,ans=0;
-        for( i=0;i<s.size();i++)
-        {
-            if(s[i]!=t[i])
-            {
-                int d=abs(s[i]-t[i]);
-                st.push(i);
-                no+=d;
-                while(!st.empty()&&(no>ma))
-                {
-                    int k=st.front();
-                    st.pop();
-                    no-=abs(s[k]-t[k]);
-                 //   cout<<k<<" ";
-                }
-                
-                if(st.empty())
-                    j=i;
-                if(!st.empty()&&no<=ma)
-                {
-                    j=st.front();
-                   // cout<<j<<" D< ";
-                     ans=max(ans,i-j+1);
-                }
-            }
-            else
-                 ans=max(ans,i-j+1);
-           
+   int equalSubstring(string s, string t, int k) {
+        int n = s.length(), i = 0, j;
+        for (j = 0; j < n; ++j) {
+            if ((k -= abs(s[j] - t[j])) < 0)
+                k += abs(s[i] - t[i++]);
         }
-        
-        return ans;
+        return j - i;
     }
 };
