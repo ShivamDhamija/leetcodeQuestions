59db78b1017ffbfc5e0ef3cfa6fa1042e0475b50
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2022-12-03 16:28:56 +0530

    Time: 3 ms (60.02%), Space: 6.1 MB (98.83%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    bool call(string s, string t,int i,int j) {
-        if(i==s.size()&&j<=t.size())
-            return true;
-        if(j==t.size())
-            return false;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        dp[i][j]=0;
-        if(s[i]==t[j])
-            dp[i][j]=call(s,t,i+1,j+1);
-        dp[i][j]|=call(s,t,i,j+1);
-        //cout<<i<<" "<<j<<" "<<dp[i][j]<<endl;
-        return dp[i][j];
-    }
     bool isSubsequence(string s, string t) {
-        dp.resize(s.size(),vector<int>(t.size(),-1));
-        return call(s,t,0,0);
-     
+        int n = s.length(),m=t.length();
+        int j = 0; 
+   
+    for (int i = 0; i < m and j < n; i++)
+        if (s[j] == t[i])
+             j++;
+    return (j == n);
     }
 };
