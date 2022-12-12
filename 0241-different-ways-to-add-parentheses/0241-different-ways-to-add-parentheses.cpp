624c5bb5ef2bb2169ddc8613aa66df0fcf7a0f72
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2022-12-12 23:08:15 +0530

    Time: 4 ms (70.24%), Space: 11.6 MB (21.01%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,48 +1,19 @@
 class Solution {
 public:
-	vector<int> diffWaysToCompute(string input) {
-		unordered_map<string, vector<int>> dpMap;
-		return computeWithDP(input, dpMap);
-	}
-
-	vector<int> computeWithDP(string input, unordered_map<string, vector<int>> &dpMap) {
-		vector<int> result;
-		int size = input.size();
-		for (int i = 0; i < size; i++) {
-			char cur = input[i];
-			if (cur == '+' || cur == '-' || cur == '*') {
-				// Split input string into two parts and solve them recursively
-				vector<int> result1, result2;
-				string substr = input.substr(0, i);
-				// check if dpMap has the result for substr
-				if (dpMap.find(substr) != dpMap.end())
-					result1 = dpMap[substr];
-				else
-					result1 = computeWithDP(substr, dpMap);
-
-				substr = input.substr(i + 1);
-				if (dpMap.find(substr) != dpMap.end())
-					result2 = dpMap[substr];
-				else
-					result2 = computeWithDP(substr, dpMap);
-				
-				for (auto n1 : result1) {
-					for (auto n2 : result2) {
-						if (cur == '+')
-							result.push_back(n1 + n2);
-						else if (cur == '-')
-							result.push_back(n1 - n2);
-						else
-							result.push_back(n1 * n2);
-					}
-				}
-			}
-		}
-		// if the input string contains only number
-		if (result.empty())
-			result.push_back(atoi(input.c_str()));
-		// save to dpMap
-		dpMap[input] = result;
-		return result;
-	}
+    vector<int> diffWaysToCompute(string input) {
+    vector<int> res;
+    for (int i = 0; i < input.size(); ++i) {
+        if (input[i] < '0') {
+            vector<int> v1 = diffWaysToCompute(input.substr(0, i));
+            vector<int> v2 = diffWaysToCompute(input.substr(i + 1));
+            for (auto i1 : v1) for (auto i2 : v2) switch (input[i]) {
+                case '+': res.push_back(i1 + i2); break;
+                case '-': res.push_back(i1 - i2); break;
+                case '*': res.push_back(i1 * i2);
+            }
+        }
+    }
+    if (res.empty()) res.push_back(stoi(input));
+    return res;
+}
 };
