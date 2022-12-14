98b29f435903c752d760a8331bfe9d3fcaac2297
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2022-12-14 15:34:03 +0530

    Time: 178 ms (96.86%), Space: 91.1 MB (71.99%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,31 +1,9 @@
 class Solution {
 public:
-     vector<vector<long long>>dp;
-    long long call(vector<int>&p,int i,int j)
-    {
-        if(i==p.size())
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-       // cout<<value<<" ";
-        if(j==0)
-        {
-            dp[i][j]=call(p,i+1,0);
-            long long d=call(p,i+1,1)+p[i];
-            if(dp[i][j]<d)
-                dp[i][j]=d;
-        }
-        else if(j==1)
-        {
-            dp[i][j]=call(p,i+1,0)-p[i];
-            long long d=call(p,i+1,j);
-            if(dp[i][j]<d)
-                dp[i][j]=d;
-        }
-        return dp[i][j];
-    }
-    long long maxAlternatingSum(vector<int>& p) {
-        dp.resize(p.size(),vector<long long>(3,-1));
-        return call(p,0,0);
+     long long maxAlternatingSum(vector<int>& A) {
+        long res = A[0];
+        for (int i = 1; i < A.size(); ++i)
+            res += max(A[i] - A[i-1], 0);
+        return res;
     }
 };
