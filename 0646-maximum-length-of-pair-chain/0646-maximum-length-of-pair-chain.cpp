be394fedfb03fd556bb3685d2606dd5c1b355d21
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2022-12-16 17:05:57 +0530

    Time: 76 ms (90.45%), Space: 22.5 MB (88.67%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
-    unordered_map<int,int>dp;
-    unordered_map<int,int>p;
-    int call(int i)
-    {
-        if(p.find(i)==p.end())
-            return 0;
-        if(dp.find(i)!=dp.end())
-            return dp[i];
-        int ans=1;
-        for(int j=p[i]+1;j<=1000;j++)
-        ans=max(ans,call(j)+1);
-        return dp[i]=ans;
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(), pairs.end(), cmp);
+        int cnt = 0;
+        for (int i = 0, j = 0; j < pairs.size(); j++) {
+            if (j == 0 || pairs[i][1] < pairs[j][0]) {
+                cnt++;
+                i = j;
+            }
+        }
+        return cnt;
     }
-    int findLongestChain(vector<vector<int>>& n) {
-        for(int i=0;i<n.size();i++)
-        {
-            int a=n[i][0],b=n[i][1];
-            if(p.find(a)==p.end())
-                p[a]=b;
-            p[a]=min(p[a],b);
-                
-        }
-        int ans=0;
-        for(int i=-1000;i<=1000;i++)
-        {
-            ans=max(ans,call(i));
-        }
-        return ans; 
+
+private:
+    static bool cmp(vector<int>& a, vector<int>&b) {
+        return a[1] < b[1];
     }
 };
