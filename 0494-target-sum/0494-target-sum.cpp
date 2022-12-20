c67d6ebac45344b0a78e9395cf0386368815cf8a
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2022-12-20 22:09:41 +0530

    Time: 36 ms (59.44%), Space: 43.9 MB (22.79%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
+    vector<vector<int>>v;
     int call(vector<int>&n,int t,int s,int i)
     {
         if(i==n.size())
         {
             return t==s;
         }
+        if(v[i][s+1000]!=-1)
+            return v[i][s+1000];
         int ans=call(n,t,s+n[i],i+1);
         ans+=call(n,t,s-n[i],i+1);
         
-        return ans;
+        return v[i][s+1000]= ans;
     }
     int findTargetSumWays(vector<int>& n, int t) {
-       return call(n,t,0,0);
+       v.resize(n.size(),vector<int>(2500,-1));
+        return call(n,t,0,0);
     }
 };
