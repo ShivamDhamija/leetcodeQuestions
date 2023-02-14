842f9a90e0fe98d87a5ed93742568a580c37c169
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2023-02-14 23:36:05 +0530

    Time: 459 ms (71.09%), Space: 117.3 MB (71.75%) - LeetHub

--- 2564-substring-xor-queries/2564-substring-xor-queries.cpp
+++ 2564-substring-xor-queries/2564-substring-xor-queries.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> substringXorQueries(string s, vector<vector<int>>& qs) {
-    vector<vector<int>> res(qs.size(), {-1, -1});
+    vector<vector<int>> substringXorQueries(string s, vector<vector<int>>& queries) {
     unordered_map<int, vector<int>> m;
-    for (int i = 0; i < qs.size(); ++i)
-        m[qs[i][0] ^ qs[i][1]].push_back(i);
+    vector<vector<int>> res;
     for (int i = 0; i < s.size(); ++i) {
         long long val = 0;
-        for (int sz = 0; sz < 30 && i + sz < s.size(); ++sz) {
-            val = (val << 1) + (s[i + sz] == '1');
-            if (auto it = m.find(val); it != end(m)) {
-                for (auto idx : it->second)
-                    res[idx] = {i, i + sz};
-                m.erase(it);
-            }
+        for (int j = i; j < min(i + 30, (int)s.size()); ++j) {
+            val = (val << 1) + (s[j] == '1');
+            if (m.count(val) == 0)
+                m[val] = {i, j};
             if (s[i] == '0')
                 break;
         }
     }
+    for (const auto &q : queries)
+        if (auto it = m.find(q[0] ^ q[1]); it != end(m))
+            res.push_back(it->second);
+        else
+            res.push_back({-1, -1});
     return res;
 }
 };
