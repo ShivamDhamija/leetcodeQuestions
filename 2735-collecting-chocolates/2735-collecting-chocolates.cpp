66d35eacf056d9b453b4f8b1f6f5b7c09a910829
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2023-06-17 18:55:36 +0530

    Time: 215 ms (92.98%), Space: 60.8 MB (70.56%) - LeetHub

---
+++ 2735-collecting-chocolates/2735-collecting-chocolates.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+     long long minCost(vector<int>& A, int x) {
+        int n = A.size();
+        vector<long long> res(n);
+        for (int i = 0; i < n; i++) {
+            res[i] += 1L * i * x;
+            int cur = A[i];
+            for (int k = 0; k < n; k++) {
+                cur = min(cur, A[(i - k + n) % n]);
+                //cout<<A[(i - k + n) % n]<<" ";
+                // cout<<cur<<" ";
+                res[k] += cur;
+            }
+            // cout<<endl;
+        }
+        return *std::min_element(res.begin(), res.end());
+    }
+};
