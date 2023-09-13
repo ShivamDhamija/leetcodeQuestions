89b24b292f4331f0ae613016d56e8ff2b8a70153
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2023-09-13 23:10:53 +0530

    Time: 3 ms (66.86%), Space: 6.6 MB (13.35%) - LeetHub

--- 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.cpp
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int addMinimum(string w) {
-        int a=0,p=0;
-        vector<vector<int>>v(50,vector<int>(3));
-        v[0][w[0]-'a']++;
-        for(int i=1;i<w.size();i++)
-        {
-            if(w[i]<=w[i-1])
-                p++;
-            v[p][w[i]-'a']++;
-        }
-        for(int i=0;i<=p;i++)
-        {
-            for(int j=0;j<3;j++)
-                if(v[i][j]==0)
-                    a++;
-        }
-        return a;
+        int no=0;
+        char c='c';
+        for(auto i:w)            
+            {
+                no+=i<=c;
+                c=i;
+            }
+        return no*3-w.size();   
     }
 };
