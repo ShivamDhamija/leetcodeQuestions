321332f2d758342844d4d4f031bc990c9ed60b9c
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2023-12-29 20:25:28 +0530

    Time: 242 ms (7.90%), Space: 89.8 MB (60.42%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -2,18 +2,25 @@ class Solution {
 public:
     int maximumScore(vector<int>& n, int k) {
         int a=n[k],i=k,j=k,m=n[k];
+//         while(i>0||j<n.size()-1)
+//         {
+//             if(i==0)
+//                 j++;
+//             else if(j==n.size()-1)
+//                 i--;
+//             else if(n[i-1]<n[j+1])
+//                 j++;
+//             else i--;
+            
+//             m=min(m,min(n[i],n[j]));
+//             a=max(a,m*(j-i+1));
+//         }
         while(i>0||j<n.size()-1)
         {
-            if(i==0)
-                j++;
-            else if(j==n.size()-1)
-                i--;
-            else if(n[i-1]<n[j+1])
-                j++;
-            else i--;
-            
+            i>0? j<n.size()-1? (n[i-1]>n[j+1]?i--:j++) :i-- :j++;
             m=min(m,min(n[i],n[j]));
             a=max(a,m*(j-i+1));
+            
         }
         return a;
     }
