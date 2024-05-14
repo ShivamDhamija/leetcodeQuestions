9358dd8bb6c90b6e8aa23078e9dd2ce4dc19dc7e
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2024-05-14 22:19:31 +0530

    Time: 2 ms (48.93%), Space: 7.5 MB (78.70%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void addLeftString(string &ans, string &w,int &i)
+    {
+        while(i<w.length())
+            ans.push_back(w[i]),i++;        
+    }
+    string mergeAlternately(string w1, string w2) {
+        int p1=0,p2=0;
+        string ans="";
+        while(p1<w1.length()&&p2<w2.length())
+        ans.push_back(w1[p1]),ans.push_back(w2[p2]),p1++,p2++;
+        addLeftString(ans,w1,p1);
+        addLeftString(ans,w2,p2);
+        return ans;
+    }
+};
