384a853be5a1afc452dadd7701b7b7fa36ba7a7b
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2024-05-14 22:25:03 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (44.87%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -8,10 +8,10 @@ public:
     string mergeAlternately(string w1, string w2) {
         int p1=0,p2=0;
         string ans="";
-        while(p1<w1.length()&&p2<w2.length())
-        ans.push_back(w1[p1]),ans.push_back(w2[p2]),p1++,p2++;
-        addLeftString(ans,w1,p1);
-        addLeftString(ans,w2,p2);
+        while(p1<w1.length()||p2<w2.length())
+        { if(p1<w1.length()){ans.push_back(w1[p1]);}if(p2<w2.length()){ans.push_back(w2[p2]);}p1++;p2++;}
+        //addLeftString(ans,w1,p1);
+        //addLeftString(ans,w2,p2);
         return ans;
     }
 };
