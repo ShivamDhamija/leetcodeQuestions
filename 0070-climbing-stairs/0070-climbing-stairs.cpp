3fa7fb2f4c6a3f3f9279a6bd3bd4bd22e5112b02
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2024-07-05 21:08:51 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (32.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<int>v;
-    int call(int n,int i)
+    int c(int n, int i)
     {
-        if(i>=n)return 1;
+        if(i==n)return 1;
+        if(i>n)return 0;
         if(v[i]!=-1)return v[i];
-        return v[i]=call(n,i+1)+call(n,i+2);
+        return v[i]=c(n,i+1)+c(n,i+2);
     }
     int climbStairs(int n) {
         v.resize(n,-1);
-        return call(n,1);
+        return c(n,0);
     }
 };
