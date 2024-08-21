de0d8e2645466c7be842ff68147ef209d9e9593a
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2024-08-21 22:32:12 +0530

    Time: 7 ms (83.20%), Space: 17.4 MB (6.90%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& m) {
-        int n=m.size(),ma=m[0].size();
-        vector<int>r(n,-1);
-        vector<int>c(ma,-1);
-        for(int i=0;i<n;i++)
-            for(int j=0;j<ma;j++)
-                if(m[i][j]==0)
-                {  
-                    r[i]=0;
-                    c[j]=0;
-                }
-        
-        for(int i=0;i<n;i++)
-            if(r[i]==0)
-            for(int j=0;j<ma;j++)
+        unordered_set<int>r;
+        unordered_set<int>c;
+        for(int i=0;i<m.size();i++)
+            for(int j=0;j<m[i].size();j++)
+                if(m[i][j]==0)r.insert(i),c.insert(j);
+        for(auto i:r)
+            for(int j=0;j<m[i].size();j++)
                 m[i][j]=0;
-            
-        for(int j=0;j<ma;j++)
-            if(c[j]==0)
-            for(int i=0;i<n;i++)
+        for(auto j:c)
+            for(int i=0;i<m.size();i++)
                 m[i][j]=0;
-        
     }
 };
