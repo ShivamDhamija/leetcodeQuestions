09ef3e0bf5ca94805c1df03bf18cadc52b425272
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2024-11-27 17:56:11 +0530

    Time: 0 ms (100.00%), Space: 24.6 MB (7.34%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cpp
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int numberOfEmployeesWhoMetTarget(vector<int>& h, int t) {
         int a=0;
-        for(auto i:h)if(i>=t)a++;
+        for(auto i:h){
+            if(i>=t)a++;
+        }
         return a;
     }
 };
