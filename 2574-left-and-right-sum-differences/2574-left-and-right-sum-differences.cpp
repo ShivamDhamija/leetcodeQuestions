73dab06574e69c9459c1f7f8d34774e086efe2d1
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2024-11-28 21:55:42 +0530

    Time: 0 ms (100.00%), Space: 15.1 MB (7.16%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    vector<int> leftRigthDifference(vector<int>& n) {
-        vector<int>l(n.size(),0);
-        vector<int>r(n.size(),0);
-        for(int i=n.size()-2;i>=0;i--)
-            r[i]=r[i+1]+n[i+1];
-        l[0]=r[0];
-        int d=n[0];
-       // cout<<d<<" ";
-        for(int i=1;i<n.size();i++)
-        {
-           // cout<<r[i]<<" ";
-            l[i]=abs(d-r[i]);
-                d+=n[i];
-         }
-        return l;
+    vector<int> leftRightDifference(vector<int>& n) {
+        int v=0;
+        for(auto i:n)v+=i;
+        vector<int>a;
+        int j=0;
+        for(auto i:n){
+            v-=i;
+            a.push_back(abs(v-j));
+            j+=i;
+        }
+        return a;
     }
 };
