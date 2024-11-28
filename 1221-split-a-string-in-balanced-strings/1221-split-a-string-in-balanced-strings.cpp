ad58bdc852a2b2fd66c4486b20e16a30280804ca
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2024-11-28 23:04:27 +0530

    Time: 2 ms (5.36%), Space: 8.4 MB (10.76%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int balancedStringSplit(string s) {
-        int c=0,r=0,l=0;
+       int c=0,r=0,l=0;
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='R')
