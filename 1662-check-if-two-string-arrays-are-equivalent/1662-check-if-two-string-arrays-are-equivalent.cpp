0573713e82b7294aba61e251f792cf9711094e1e
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2024-11-30 22:14:04 +0530

    Time: 0 ms (100.00%), Space: 15 MB (8.26%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -3,14 +3,9 @@ public:
     bool arrayStringsAreEqual(vector<string>& w1, vector<string>& w2) {
         string s1="",s2="";
         for(auto i:w1)
-            s1+=i;
+            for(auto j:i)s1.push_back(j);
         for(auto i:w2)
-            s2+=i;
-        if(s1.size()!=s2.size())return 0;
-        for(int i=0,j=0;i<s1.size()&&j<s2.size();i++,j++)
-        {
-            if(s1[i]!=s2[j])return 0;
-        }
-        return 1;
+            for(auto j:i)s2.push_back(j);
+        return s1==s2;
     }
 };
