504278ef28eb9e285bb3a2f86d2d21eacb7419f3
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2024-12-02 22:19:06 +0530

    Time: 17 ms (32.85%), Space: 69.6 MB (45.79%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& n1, vector<int>& n2) {
+        unordered_map<int,int>m1;
+        unordered_map<int,int>m2;
         int a=0,b=0;
-        unordered_set<int>m1,m2;
-        for(auto i:n1)
-            m1.insert(i);
-        for(auto i:n2)
-            m2.insert(i);
-        for(auto i:n1)
-            if(m2.find(i)!=m2.end())
-                a++;
-        for(auto i:n2)
-            if(m1.find(i)!=m1.end())
-                b++;
+        for(auto i:n1)m1[i]++;
+        for(auto i:n2)m2[i]++;
+        for(auto i:m1){
+            if(m2.find(i.first)!=m2.end())a+=m1[i.first];
+        }
+        for(auto i:m2){
+            if(m1.find(i.first)!=m1.end())b+=m2[i.first];
+        }
         return {a,b};
     }
 };
