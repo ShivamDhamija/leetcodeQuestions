ff56bb4fb746321a3d5a917c2a70794216caf201
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2024-12-02 22:05:05 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (7.00%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]>='A'&&s[i]<='Z')
-            {
-                int j=s[i]-'A';
-                s[i]=j+'a';
-            }
+        for(int i=0;i<s.size();i++){
+            if(s[i]>='A'&&s[i]<='Z')s[i]=s[i]-'A'+'a';
         }
         return s;
     }
