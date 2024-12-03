de1021c29e2f9c62a212d63cda11d755146e06a2
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2024-12-03 16:13:26 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (94.68%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.cpp
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -3,10 +3,13 @@ public:
     string finalString(string s) {
         string a="";
         for(auto i:s)
+        {
             if(i=='i')
                 reverse(a.begin(),a.end());
             else
                 a.push_back(i);
+        }
+            
         return a;
     }
 };
