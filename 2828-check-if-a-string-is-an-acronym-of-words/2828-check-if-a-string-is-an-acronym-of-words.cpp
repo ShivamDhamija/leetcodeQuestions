e1e4969beca898eb94479758c0f40aef13b8b0ef
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2024-12-04 23:04:13 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (22.60%) - LeetHub

--- 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.cpp
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     bool isAcronym(vector<string>& w, string s) {
-        if(w.size()!=s.size())return false;
+        if(s.size()!=w.size())return false;
         for(int i=0;i<s.size();i++)
-        {
-            if(s[i]!=w[i][0])return false;
-        }
+            if(s[i]!=w[i][0])return false;        
         return true;
     }
 };
