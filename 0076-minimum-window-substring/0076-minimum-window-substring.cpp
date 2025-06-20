a6bb2460fda29e92b0e14a330e1da1b20369dbf0
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2025-06-20 22:49:44 +0530

    Time: 0 ms (100%), Space: 11.3 MB (83.97%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int> map(128,0);
-        for (char c : t) {
-            map[c]++;
-        }
-
-        int counter = t.size(), begin = 0, end = 0, d = INT_MAX, head = 0;
-        while (end < s.size()){
-            if (map[s[end++]]-- > 0) {
-                counter--;
+        vector<int>m(128,0);
+        for(auto i:t)m[i]++;
+        int c=t.size(),begin=0,end=0, a=0,d=INT_MAX;
+        while(end<s.size()){
+            c-= m[s[end++]]-- >0 ?1:0;
+            while(c==0){
+                if(d>end-begin){
+                    d=end-begin;
+                    a=begin;
+                }
+                c+= m[s[begin ++]]++ ==0 ?1:0;
             }
-            while (counter == 0) {
-                if (end - begin < d) {
-                    head = begin;
-                    d = end - head;
-                }
-                if (map[s[begin]]++ == 0) {
-                    counter++;
-                }
-                begin++;
-            }  
         }
-        return d == INT_MAX ? "" : s.substr(head, d);
+        return d==INT_MAX?"":s.substr(a,d);
     }
 };
