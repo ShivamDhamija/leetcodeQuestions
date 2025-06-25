9cd2367eeb4c3185f04ed0e2b2d1c2d101754955
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2025-06-25 20:29:16 +0530

    Time: 10 ms (48.65%), Space: 28.3 MB (19.44%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    int m, n;
-	// denotes cells reachable starting from atlantic and pacific edged cells respectively
-    vector<vector<bool> > atlantic, pacific;
-	vector<vector<int> > ans;    
-    vector<vector<int> > pacificAtlantic(vector<vector<int>>& mat) {
-        if(!size(mat)) return ans;
-        m = size(mat), n = size(mat[0]);
-        atlantic = pacific = vector<vector<bool> >(m, vector<bool>(n, false));
-		// perform dfs from all edge cells (ocean-connected cells)
-        for(int i = 0; i < m; i++) dfs(mat, pacific, i, 0), dfs(mat, atlantic, i, n - 1);
-        for(int i = 0; i < n; i++) dfs(mat, pacific, 0, i), dfs(mat, atlantic, m - 1, i);             
+    vector<vector<int>>p;
+    vector<vector<int>>a;
+    vector<vector<int>>ans;
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        if(heights.size()==0)return ans;
+        int n=heights.size(),m=heights[0].size();
+        p.resize(n,vector<int>(m,0));
+        a.resize(n,vector<int>(m,0));
+        for(int i=0;i<n;i++)help(heights,p,i,0,n,m),help(heights,a,i,m-1,n,m);
+        for(int j=0;j<m;j++)help(heights,p,0,j,n,m),help(heights,a,n-1,j,n,m);
         return ans;
     }
-    void dfs(vector<vector<int> >& mat, vector<vector<bool> >& visited, int i, int j){        
-        if(visited[i][j]) return;
-        visited[i][j] = true;
-		// if cell reachable from both the oceans, insert into final answer vector
-        if(atlantic[i][j] && pacific[i][j]) ans.push_back(vector<int>{i, j});    
-		// dfs from current cell only if height of next cell is greater
-/*⬇️*/  if(i + 1 <  m && mat[i + 1][j] >= mat[i][j]) dfs(mat, visited, i + 1, j); 
-/*⬆️*/  if(i - 1 >= 0 && mat[i - 1][j] >= mat[i][j]) dfs(mat, visited, i - 1, j);
-/*➡️*/  if(j + 1 <  n && mat[i][j + 1] >= mat[i][j]) dfs(mat, visited, i, j + 1); 
-/*⬅️*/  if(j - 1 >= 0 && mat[i][j - 1] >= mat[i][j]) dfs(mat, visited, i, j - 1);
+    void help(vector<vector<int>>&h,vector<vector<int>>&v,int i,int j,int &n, int &m){
+        if(v[i][j]==1)return;
+        v[i][j]=1;
+        if(p[i][j] && a[i][j])ans.push_back({i,j});
+        if(i>0&& h[i-1][j]>=h[i][j])help(h,v,i-1,j,n,m);
+        if(i<n-1&& h[i+1][j]>=h[i][j])help(h,v,i+1,j,n,m);
+        if(j>0&& h[i][j-1]>=h[i][j])help(h,v,i,j-1,n,m);
+        if(j<m-1&& h[i][j+1]>=h[i][j])help(h,v,i,j+1,n,m);
     }
 };
