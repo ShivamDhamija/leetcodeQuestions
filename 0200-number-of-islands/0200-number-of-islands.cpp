12c7e198d7e84ce084f49d4aa3a2fa87d0e7ce60
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2025-06-25 19:11:50 +0530

    Time: 28 ms (52.04%), Space: 16.3 MB (68.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
-    int n,m;
-    bool c1(vector<vector<char>>& g,vector<vector<int>>&vis,int i,int j)
-    {
-        if(i<0||i>=n||j<0||j>=m)return false;
-        if(vis[i][j]==1||g[i][j]=='0')return false;
-        vis[i][j]=1;
-        return true;
+    void clear(vector<vector<char>>&g,int i,int j,int &I,int &J){
+        if(i<0 || j<0 ||i==I || j==J ||g[i][j]=='0')return ;
+        g[i][j]='0';
+        clear(g,i+1,j,I,J);
+        clear(g,i,j+1,I,J);
+        clear(g,i-1,j,I,J);
+        clear(g,i,j-1,I,J);
     }
-    
-    void dfs(vector<vector<char>>& g,vector<vector<int>>&vis,int i,int j)
-    {
-        vector<vector<int>>v={{1,0},{-1,0},{0,1},{0,-1}};
-        for(int I=0;I<4;I++)
-        if(c1(g,vis,i+v[I][0],j+v[I][1]))
-        {
-            dfs(g,vis,i+v[I][0],j+v[I][1]);
+    int numIslands(vector<vector<char>>& grid) {
+        int a=0,I=grid.size(),J=grid[0].size();
+        for(int i=0;i<I;i++)
+        for(int j=0;j<J;j++)
+        if(grid[i][j]=='1'){
+            a++;
+            clear(grid,i,j,I,J);
         }
-    }
-    
-    int numIslands(vector<vector<char>>& g) {
-        n=g.size();
-        m=g[0].size();
-        int ans=0;
-        
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                if(c1(g,vis,i,j))
-                {
-                    ans++;
-                    dfs(g,vis,i,j);
-                   // cout<<i<<""<<j<<endl;
-                }
-        
-        return ans;
+        return a;
     }
 };
