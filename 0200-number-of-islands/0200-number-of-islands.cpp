a0a8c28aff38a340a9054ca6f4b52fbc5fc84580
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2025-06-26 22:10:47 +0530

    Time: 26 ms (65.06%), Space: 16.4 MB (66.58%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    void clear(vector<vector<char>>&g,int i,int j,int &I,int &J){
-        if(i<0 || j<0 ||i==I || j==J ||g[i][j]=='0')return ;
+    void help(vector<vector<char>>&g,int i,int j,int &I,int &J){
+        if(i<0||j<0||i==I||j==J||g[i][j]=='0')return ;
         g[i][j]='0';
-        clear(g,i+1,j,I,J);
-        clear(g,i,j+1,I,J);
-        clear(g,i-1,j,I,J);
-        clear(g,i,j-1,I,J);
+        help(g,i+1,j,I,J);
+        help(g,i-1,j,I,J);
+        help(g,i,j+1,I,J);
+        help(g,i,j-1,I,J);
     }
     int numIslands(vector<vector<char>>& grid) {
         int a=0,I=grid.size(),J=grid[0].size();
@@ -14,8 +14,9 @@ public:
         for(int j=0;j<J;j++)
         if(grid[i][j]=='1'){
             a++;
-            clear(grid,i,j,I,J);
+            help(grid,i,j,I,J);
         }
+
         return a;
     }
 };
