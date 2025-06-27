fca3e2c17aeb951442676fc2847b720b1a089467
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2025-06-27 18:40:06 +0530

    Time: 0 ms (100%), Space: 15.1 MB (94.98%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,15 +6,13 @@ public:
             m=(l+r)/2;
             if(nums[m]==target)return m;
             if(nums[l]<=nums[m]){
-                if(nums[l]<=target&&target<nums[m])
+                if(nums[l]<=target && target<nums[m])
                 r=m-1;
-                else
-                l=m+1;
+                else l=m+1;
             }else{
-                if(nums[m]<target&&target<=nums[r])
+                if(nums[m]<target && target<=nums[r])
                 l=m+1;
-                else
-                r=m-1;
+                else r=m-1;
             }
         }
         return nums[l]==target?l:-1;
