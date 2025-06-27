b56c1d7df5a8c793fd7978e5ea748b9f5e5d5311
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2025-06-27 19:11:22 +0530

    Time: 10 ms (48.41%), Space: 28.1 MB (20.02%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>>p;
-    vector<vector<int>>a;
-    vector<vector<int>>ans;
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        if(heights.size()==0)return ans;
-        int n=heights.size(),m=heights[0].size();
-        p.resize(n,vector<int>(m,0));
-        a.resize(n,vector<int>(m,0));
-        for(int i=0;i<n;i++)help(heights,p,i,0,n,m),help(heights,a,i,m-1,n,m);
-        for(int j=0;j<m;j++)help(heights,p,0,j,n,m),help(heights,a,n-1,j,n,m);
+    vector<vector<int>>p,a,ans;
+    int n,m;
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {        
+        n=heights.size();
+        m=heights[0].size();
+        p.resize(n,vector<int>(m));
+        a.resize(n,vector<int>(m));
+        for(int i=0;i<n;i++)help(heights,p,i,0) , help(heights,a,i,m-1);
+        for(int i=0;i<m;i++)help(heights,p,0,i) , help(heights,a,n-1,i);
         return ans;
     }
-    void help(vector<vector<int>>&h,vector<vector<int>>&v,int i,int j,int &n, int &m){
-        if(v[i][j]==1)return;
+    void help(vector<vector<int>>&h,vector<vector<int>>&v,int i,int j){
+        if(v[i][j])return;
         v[i][j]=1;
         if(p[i][j] && a[i][j])ans.push_back({i,j});
-        if(i>0&& h[i-1][j]>=h[i][j])help(h,v,i-1,j,n,m);
-        if(i<n-1&& h[i+1][j]>=h[i][j])help(h,v,i+1,j,n,m);
-        if(j>0&& h[i][j-1]>=h[i][j])help(h,v,i,j-1,n,m);
-        if(j<m-1&& h[i][j+1]>=h[i][j])help(h,v,i,j+1,n,m);
+        if(i>0   && h[i][j]<=h[i-1][j])help(h,v,i-1,j);
+        if(i+1<n && h[i][j]<=h[i+1][j])help(h,v,i+1,j);
+        if(j>0   && h[i][j]<=h[i][j-1])help(h,v,i,j-1);
+        if(j+1<m && h[i][j]<=h[i][j+1])help(h,v,i,j+1);
     }
+
 };
