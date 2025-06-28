e5af050db56d43418f746855e256efefd05450f2
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2025-06-28 16:07:20 +0530

    Time: 11 ms (12.02%), Space: 19.3 MB (10.49%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>d(numCourses);
+        unordered_map<int,vector<int>>m;
+        vector<int>a;
+        for(auto i:prerequisites){
+            d[i[0]]++;
+            m[i[1]].push_back(i[0]);
+        }
+        queue<int>q;
+        for(int i=0;i<numCourses;i++)if(d[i]==0)q.push(i);
+        while(!q.empty()){
+            int f=q.front();
+            a.push_back(f);
+            q.pop();
+            for(auto i:m[f]){d[i]--;if(d[i]==0)q.push(i);}
+        }
+        for(auto i:d)if(i!=0)return {};
+        return a;
+    }
+};
