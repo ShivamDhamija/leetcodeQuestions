569f3bdeb23ae1cff9715acefc576277078b39f8
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2025-07-02 19:08:41 +0530

    Time: 51 ms (53.39%), Space: 29.1 MB (73.56%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>>a;
         for(int i=0;i<nums.size();i++){
-            if(i>0&&nums[i]==nums[i-1])continue;
+            if(i>0&&nums[i-1]==nums[i])continue;
             int j=i+1,k=nums.size()-1;
             while(j<k){
                 if(nums[i]+nums[j]+nums[k]==0){
@@ -12,7 +12,7 @@ public:
                     while(j<k&&nums[j]==nums[++j]);
                     while(j<k&&nums[k]==nums[--k]);
                 }
-                else if(nums[i]+nums[j]+nums[k]<0)j++;
+                else if(nums[i]+nums[k]+nums[j]<0)j++;
                 else k--;
             }
         }
