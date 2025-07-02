8dbcabb5cd0b7b6395916102c39ae100e3dd8840
shivam dhamija <61574426+ShivamDhamija@users.noreply.github.com>
2025-07-02 19:14:23 +0530

    Time: 21 ms (91.92%), Space: 16.1 MB (89.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    void help(vector<vector<char>>&g,int i,int j,int &n,int &m){
-        if(i<0||j<0||i==n||j==m||g[i][j]=='0')return ;
+    void help(vector<vector<char>>&g ,int i,int j,int I,int J){
+        if(i<0|| j<0||i==I||j==J||g[i][j]=='0')return ;
         g[i][j]='0';
-        help(g,i-1,j,n,m);
-        help(g,i+1,j,n,m);
-        help(g,i,j-1,n,m);
-        help(g,i,j+1,n,m);
+        help(g,i-1,j,I,J);
+        help(g,i+1,j,I,J);
+        help(g,i,j-1,I,J);
+        help(g,i,j+1,I,J);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int a=0,n=grid.size(),m=grid[0].size();
-        for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
-        if(grid[i][j]=='1'){
-            a++; help(grid,i,j,n,m);
-        }
+        int a=0;
+        for(int i=0;i<grid.size();i++)
+        for(int j=0;j<grid[0].size();j++)
+        if(grid[i][j]=='1')a++,help(grid,i,j,grid.size(),grid[0].size());
         return a;
     }
 };
